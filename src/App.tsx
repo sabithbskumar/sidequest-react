587ceaa9d9b677c2c1495a0a9e05cb25e0d3d666
Sabith <65082465+sabithbskumar@users.noreply.github.com>
2024-01-10 08:37:35 +0530

    Chore: Renamed delete button

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
                         });
                       }}
                     >
-                      x
+                      Delete
                     </button>
                   </div>
                 );
