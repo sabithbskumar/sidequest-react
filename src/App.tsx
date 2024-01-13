79a5557f28cd66d3219c94b4247f76900096c582
Sabith <65082465+sabithbskumar@users.noreply.github.com>
2024-01-13 14:42:39 +0530

    Refactor: Extracted app header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
+import { Header } from "./components/Header";
 import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
     <div className="bg-neutral-800 text-neutral-50 flex flex-col h-full">
-      <header className="bg-neutral-600 shrink-0">
-        <div className="py-2">
-          <span className="text-3xl px-6">SideQuest</span>
-        </div>
-      </header>
+      <Header />
       <div className="grow overflow-hidden">
         <TodoList />
       </div>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-neutral-600 shrink-0">
+      <div className="py-2">
+        <span className="text-3xl px-6">SideQuest</span>
+      </div>
+    </header>
+  );
+}
+
+export { Header };
