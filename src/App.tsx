0aa380e9cfb44e3fdee3a8d75b1d291ff100a73a
Sabith <65082465+sabithbskumar@users.noreply.github.com>
2024-01-14 20:50:56 +0530

    UI: Improved sidebar style

--- src/App.tsx
+++ src/App.tsx
@@ -27,36 +27,38 @@ function App() {
         <div
           className={`h-full landscape:w-full max-w-none md:max-w-xs landscape:max-w-xs overflow-hidden overflow-y-auto ${
             isSidebarVisible ? "portrait:w-8/12" : "portrait:w-0"
-          } shrink-0 min-w-0 transition-[width] bg-neutral-800 flex flex-col gap-2`}
+          } shrink-0 min-w-0 transition-[width] bg-neutral-800`}
         >
-          <div className="pb-5">
-            <Header />
+          <div className="portrait:ml-4 flex flex-col gap-2">
+            <div className="py-4 px-2">
+              <Header />
+            </div>
+            <button
+              className={`mx-2 ${
+                page === "quests" ? "bg-blue-500" : "bg-neutral-600"
+              }`}
+              onClick={() => {
+                setPage("quests");
+              }}
+            >
+              Quests
+            </button>
+            <button
+              className={`mx-2 ${
+                page === "currency" ? "bg-blue-500" : "bg-neutral-600"
+              }`}
+              onClick={() => {
+                setPage("currency");
+              }}
+            >
+              Currency
+            </button>
           </div>
-          <button
-            className={`mx-2 ${
-              page === "quests" ? "bg-blue-500" : "bg-neutral-600"
-            }`}
-            onClick={() => {
-              setPage("quests");
-            }}
-          >
-            Quests
-          </button>
-          <button
-            className={`mx-2 ${
-              page === "currency" ? "bg-blue-500" : "bg-neutral-600"
-            }`}
-            onClick={() => {
-              setPage("currency");
-            }}
-          >
-            Currency
-          </button>
         </div>
         <div
           className={`w-full portrait:shrink-0 bg-neutral-700 p-2 transition-[margin_border] ${
             isSidebarVisible
-              ? "portrait:rounded-3xl portrait:m-8"
+              ? "portrait:rounded-3xl portrait:m-4"
               : "portrait:m-0"
           } overflow-hidden overflow-y-auto max-w-full`}
           onClick={() => {
